8ddb916738f363a7d4a9c927269fdb4222440cb1
Vishu51 <vishal.gupta@diatoz.com>
2023-05-23 14:29:41 +0530

    Deleted service folder.

--- src/services/helper/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-export default axios.create({
-  baseURL: 'https://20.249.98.2:8712/',
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json;charset=UTF-8'
-  }
-})
